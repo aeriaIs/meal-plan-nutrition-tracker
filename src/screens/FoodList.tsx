2a0969a4b23b02f93750da9183adc5665019f30b
aerials <nosvengeance@gmail.com>
2023-11-21 20:07:08 +0700

    feat: finishing Food List UI

--- src/screens/FoodList.tsx
+++ src/screens/FoodList.tsx
@@ -1,23 +1,21 @@
-import {View} from 'react-native';
+import {FlatList, View} from 'react-native';
 
-import {
-  BasicDivider,
-  CollapsibleBox,
-  CustomButton,
-  OrdinaryText,
-} from '../components/common';
-import {HeaderBar, Section} from '../components/core';
+import {CollapsibleBox, CustomButton, OrdinaryText} from '../components/common';
+import {HeaderBar, ScreenContainer, Section} from '../components/core';
 
 import {theme} from '../utils/theme';
 
 import useFoodStore from '../stores/foodStore';
-import {Fragment} from 'react';
 
 const FoodListScreen = ({navigation}: {navigation: any}) => {
   const {list: foodList} = useFoodStore();
+  console.log(
+    '🚀 ~ file: FoodList.tsx:17 ~ FoodListScreen ~ foodList:',
+    foodList,
+  );
 
   return (
-    <>
+    <ScreenContainer statusBar={true}>
       <HeaderBar
         title="Food List"
         customTitleStyle={{
@@ -27,100 +25,78 @@ const FoodListScreen = ({navigation}: {navigation: any}) => {
           onPress: () => navigation.goBack(),
         }}
       />
-      <Section>
-        <View style={{paddingVertical: 16, alignItems: 'center', flex: 1}}>
-          <View
-            style={{
-              marginTop: 16,
+      <Section
+        customStyle={{
+          paddingVertical: 16,
+        }}>
+        <OrdinaryText align="center">
+          List of food that you have added to your food list
+        </OrdinaryText>
+        <View style={{marginTop: 16}}>
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            bounces={false}
+            contentContainerStyle={{
+              paddingBottom: 20,
+            }}
+            data={foodList}
+            keyExtractor={(item: any) => item.id.toString()}
+            renderItem={({item, index}) => {
+              return (
+                <CollapsibleBox
+                  key={index}
+                  customContainerStyle={{
+                    borderBottomWidth: 1,
+                    borderBottomColor: theme.colors.gray[200],
+                  }}
+                  header={
+                    <OrdinaryText weight="bold">{item?.name}</OrdinaryText>
+                  }
+                  content={
+                    <View>
+                      {Object.keys(item.nutrition).map(
+                        (key: string, idx: number) => {
+                          return (
+                            <View
+                              key={idx}
+                              style={{
+                                flexDirection: 'row',
+                                justifyContent: 'space-between',
+                                alignItems: 'center',
+                                paddingVertical: 4,
+                              }}>
+                              <OrdinaryText
+                                style={{
+                                  textTransform: 'capitalize',
+                                }}>
+                                {key}
+                              </OrdinaryText>
+                              <OrdinaryText weight="semibold">
+                                {item?.nutrition[key]} {item.unit}
+                              </OrdinaryText>
+                            </View>
+                          );
+                        },
+                      )}
+                    </View>
+                  }
+                />
+              );
+            }}
+          />
+        </View>
+
+        <View style={{marginTop: 40, width: '100%'}}>
+          <CustomButton
+            text="Add Food"
+            customStyle={{
               width: '100%',
-              alignItems: 'center',
-            }}>
-            <OrdinaryText>
-              What you have eaten today? Let's track it!
-            </OrdinaryText>
-
-            <View
-              style={{
-                width: '100%',
-                marginTop: 40,
-                paddingVertical: 12,
-                paddingHorizontal: 8,
-                borderWidth: 1,
-                borderColor: theme.colors.gray[100],
-                borderRadius: 8,
-              }}>
-              {foodList.map((food: any, index: number) => {
-                console.log(
-                  '🚀 ~ file: FoodList.tsx:93 ~ {foodList.map ~ food:',
-                  food,
-                );
-
-                return (
-                  <Fragment key={index}>
-                    <CollapsibleBox
-                      customContainerStyle={{
-                        paddingVertical: 8,
-                      }}
-                      header={
-                        <OrdinaryText weight="bold" size={16}>
-                          {food.name}
-                        </OrdinaryText>
-                      }
-                      content={
-                        <View>
-                          {Object.keys(food.nutrition).map(
-                            (key: string, idx: number) => {
-                              return (
-                                <View
-                                  key={idx}
-                                  style={{
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    paddingVertical: 4,
-                                  }}>
-                                  <OrdinaryText>{key}</OrdinaryText>
-                                  <OrdinaryText>
-                                    {food.nutrition[key]}
-                                  </OrdinaryText>
-                                </View>
-                              );
-                            },
-                          )}
-                        </View>
-                      }
-                    />
-                    <BasicDivider height={1} marginVertical={0} />
-                  </Fragment>
-                );
-              })}
-            </View>
-
-            <View
-              style={{
-                marginTop: 40,
-                paddingVertical: 12,
-                paddingHorizontal: 8,
-                borderWidth: 1,
-                borderColor: theme.colors.gray[100],
-                borderRadius: 8,
-              }}>
-              <OrdinaryText>Nutrition Data</OrdinaryText>
-            </View>
-
-            <View style={{marginTop: 40, width: '100%'}}>
-              <CustomButton
-                text="Add Food"
-                customStyle={{
-                  width: '100%',
-                  fontweight: 'bold',
-                }}
-              />
-            </View>
-          </View>
+              fontweight: 'bold',
+            }}
+          />
         </View>
       </Section>
-    </>
+    </ScreenContainer>
   );
 };
 
