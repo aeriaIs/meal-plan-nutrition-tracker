304f884526556f9eee304304eba864b14cec5859
aerials <nosvengeance@gmail.com>
2023-11-21 19:54:14 +0700

    fix: update structure

--- src/components/core/ScreenContainer.tsx
+++ src/components/core/ScreenContainer.tsx
@@ -4,27 +4,55 @@ import {View, SafeAreaView, StatusBar} from 'react-native';
 type ScreenContainerProps = PropsWithChildren<{
   style?: {};
   safe?: boolean;
-  statusBar?: boolean;
+  statusBar?: any;
 }>;
 
+const marginTop = StatusBar.currentHeight || 0;
+
 const ScreenContainer = (props: ScreenContainerProps) => {
-  const {children, style, safe = true, statusBar = false} = props;
+  const {children, style, safe = true, statusBar} = props;
+  const {
+    show = true,
+    animated = true,
+    translucent = true,
+    barStyle = 'dark-content',
+    backgroundColor = 'white',
+    showHideTransition = 'fade',
+  } = statusBar;
 
   return (
     <>
-      {statusBar && (
+      {show && (
         <StatusBar
-          translucent={false}
-          barStyle={'dark-content'}
-          backgroundColor="white"
+          animated={animated}
+          translucent={translucent}
+          barStyle={barStyle}
+          backgroundColor={backgroundColor}
+          showHideTransition={showHideTransition}
         />
       )}
       {safe ? (
-        <SafeAreaView {...props} style={[styles.container, style]}>
+        <SafeAreaView
+          {...props}
+          style={[
+            styles.container,
+            {
+              marginTop: !show ? 0 : marginTop,
+            },
+            style,
+          ]}>
           {children}
         </SafeAreaView>
       ) : (
-        <View {...props} style={[styles.container, style]}>
+        <View
+          {...props}
+          style={[
+            styles.container,
+            {
+              marginTop: !show ? 0 : marginTop,
+            },
+            style,
+          ]}>
           {children}
         </View>
       )}
